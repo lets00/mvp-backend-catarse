3079800018f4a4aa5c02094855b91c163f3342a0
Luís Eduardo <eduardovansilva@gmail.com>
2020-08-31 15:07:54 -0300

    feat: create POST and DELETE routes skeleton

--- app/controllers/v1/project_controller.rb
+++ app/controllers/v1/project_controller.rb
@@ -3,4 +3,24 @@ class V1::ProjectController < ApplicationController
         @projects = Project.all
         render json: @projects, status: :ok
     end
+
+    def create
+        @project = Project.new(project_params)
+        @project.save
+        render json: @project, status: :ok
+    end
+
+    def destroy
+        @project = Project.find(params[:id])
+        if @project.destroy
+            head(:ok)
+        else
+            head(:unprocessable_entity)
+        end
+    end
+
+    private
+    def project_params
+        params.require(:project).permit(:title, :description, :image_url, :meta, :finnish_date)
+    end
 end

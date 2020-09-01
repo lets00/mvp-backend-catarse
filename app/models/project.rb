875547d6e98c547f35ae20da7df0cfe9c32fef95
Luís Eduardo <eduardovansilva@gmail.com>
2020-09-01 14:21:06 -0300

    fix: change goal to decimal and add validate role

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
     validates :title, presence: true, length: {maximum: 64}
     validates :description, presence: true, length: {maximum: 512}
     validates :image_url, presence: true
-    validates :goal, presence: true
+    validates_numericality_of :goal, greater_than: 0, less_than_or_equal_to: 500, message: 'Value must be between 0 to 500'
     validates :finnish_date, presence: true
     validates :user_id, presence: true
 end

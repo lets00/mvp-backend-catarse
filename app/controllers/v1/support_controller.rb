d042b41a72620a928c992485cb597a10b0edda6c
Luís Eduardo <eduardovansilva@gmail.com>
2020-09-01 20:27:38 -0300

    feat: add validation time in support route

--- app/controllers/v1/support_controller.rb
+++ app/controllers/v1/support_controller.rb
@@ -12,10 +12,14 @@ class V1::SupportController < ApplicationController
         @support = Support.new(json_support)
         if @support.valid?
             @project = Project.find(params[:project_id])
-            if @project
+            # Verify if finnish date was reached
+            f_finnish_date = Time.parse(@project.finnish_date).to_i
+            if Time.now.to_i <= f_finnish_date
                 @support.save
-                render json: @support, status: :ok                
-            end            
+                render json: @support, status: :ok
+            else
+                render json: {message: "Finnish date was reached"}, status: :bad_request
+            end                          
         else
             render json: @support.errors.messages, status: :bad_request
         end
